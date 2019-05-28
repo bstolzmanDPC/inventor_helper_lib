9fc90e4db79cdefc9877c94169e08b8a896874a6
Barrett Stolzman <bstolzman@doverprecision.com>
2019-05-28 14:20:32 -0500

    Add project files.

---
+++ InventorHelperLib/InventorHelper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Inventor;
+
+namespace InventorHelperLib
+{
+    static public class InventorHelper
+    {
+        static public void DerivePartFromAssembly(string AssemblyFileName, string OptionalProgramId)
+        {
+            //get running application instance
+            Application app = GetInventorObject();
+            app.Visible = true;
+
+            //Create new empty part document to derive into
+            Inventor.PartDocument doc = app.Documents.Add(DocumentTypeEnum.kPartDocumentObject) as PartDocument;
+
+            //Create derived assembly definition from assembly file
+            Inventor.DerivedAssemblyDefinition def = doc.ComponentDefinition.ReferenceComponents.DerivedAssemblyComponents.CreateDefinition(AssemblyFileName);
+
+            //Derive from definition
+            doc.ComponentDefinition.ReferenceComponents.DerivedAssemblyComponents.Add(def);
+
+            //Save document with optional program use identifier
+            doc.SaveAs($"{AssemblyFileName}_{OptionalProgramId}",false);
+
+            doc.Close();
+
+        }
+
+        static private Inventor.Application GetInventorObject()
+        {
+            try
+            {
+                return System.Runtime.InteropServices.Marshal.GetActiveObject("Inventor.Application") as Inventor.Application;
+            }
+            catch
+            {
+                Type appTpye = System.Type.GetTypeFromProgID("Inventor.Application");
+                return System.Activator.CreateInstance(appTpye) as Inventor.Application;
+            }
+        }
+        
+    }
+}
---
+++ InventorHelperLib/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("InventorHelperLib")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Waukesha Bearings Corporation")]
+[assembly: AssemblyProduct("InventorHelperLib")]
+[assembly: AssemblyCopyright("Copyright © Waukesha Bearings Corporation 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("fd2a8207-cc5d-4d6d-b3db-14d47ff4dd22")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
